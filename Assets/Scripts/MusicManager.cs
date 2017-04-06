ed488182deaec0850f2029b739678f76f335e3c9
coolk19 <khalid.talakshi@outlook.com>
2017-04-05 21:10:38 -0400

    First Add

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] musicArray;
+	private AudioSource audioSource; 
+
+	// Use this for initialization
+	void Awake () {
+		
+		DontDestroyOnLoad(gameObject);
+	
+	}
+	
+	void Start () {
+	
+		audioSource = GetComponent<AudioSource>();
+	
+	}
+
+	void OnLevelWasLoaded(int level) {
+	
+		AudioClip currentClip = musicArray[level];
+	
+		Debug.Log ("Playing Clip: " + musicArray[level]);
+		
+		if (currentClip) {
+		
+			audioSource.clip = currentClip;
+			audioSource.loop = true;
+			audioSource.Play();
+			
+		}
+	}
+}
